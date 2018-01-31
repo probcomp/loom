7bd269fd2ccec8402d5060f6b910c11a9f3eb500
Aaron Vinson <avinson.public@gmail.com>
2018-01-30 16:33:47 -0800

    add back anaconda upload to troubleshoot error

--- conda/upload.sh
+++ conda/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 
-conda install anaconda-client
+#conda install anaconda-client
 anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/loom-*.tar.bz2 --force
