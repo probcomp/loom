32227b125d45f1435ff6e6f05df76b5161158bcf
Aaron Vinson <avinson@users.noreply.github.com>
2018-02-01 02:26:14 -0800

    label package as edge and main for dependent edge builds

--- conda/upload.sh
+++ conda/upload.sh
@@ -2,4 +2,4 @@
 set -ev
 
 #conda install anaconda-client
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/loom-*.tar.bz2 --force
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l main -l edge ~/miniconda/conda-bld/linux-64/loom-*.tar.bz2 --force
