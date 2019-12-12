a1913e6de9edd0d0fda1a7e4758930418cd053a0
ulli <ulli@mit.edu>
2019-12-12 10:25:19 -0500

    Test 3 different seeds.

--- loom/test/test_infer.py
+++ loom/test/test_infer.py
@@ -1,11 +1,29 @@
 import loom.tasks
 
-def test_seeding():
-    """Test if we can seed learning"""
-    config = {'schedule': {'extra_passes': 1.0}, 'seed':42}
-    path_to_taxi = 'examples/taxi/'
-    loom.tasks.ingest(name, path_to_taxi + 'schema.json', 'example.json', debug=True)
+def _learn_seeded_taxi(seed):
+    """Helper function to learn the exact same taxi model with different seeds"""
+    config = {'schedule': {'extra_passes': 1.0}, 'seed':seed}
+    path_to_taxi = 'examples/taxi'
+    name = 'taxi-{}'.format(seed)
+    loom.tasks.ingest(
+            name,
+            '{}/schema.json'.format(path_to_taxi),
+            '{}/example.json'.format(path_to_taxi),
+            debug=True
+    )
     loom.tasks.infer(name, sample_count=0, config=config, debug=True)
     with loom.tasks.query(name) as server:
-        print server.relate(['fare_amount', 'surchare'])
+        dependencies = server.relate(['fare_amount', 'surchare'])
+    return dependencies
+
+def test_seeding():
+    """Test if we can seed learning"""
+    dependencies_1 = _learn_seeded_taxi(42)
+    dependencies_2 = _learn_seeded_taxi(42)
+    assert dependencies_1 == dependencies_2, 'Setting the seeed did not work'
+    dependencies_3 = _learn_seeded_taxi(43)
+    assert dependencies_1 != dependencies_3, 'Auto-seeding overwrites our seed'
     raise ValueError('this worked!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+
+
